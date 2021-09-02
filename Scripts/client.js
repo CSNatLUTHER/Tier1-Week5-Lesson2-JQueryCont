8f4ef249485a687557b1123d605a37df08e9f8c3
Chris Nelson <cnelson04@gmail.com>
2021-09-01 20:29:25 -0500

    new button there

--- Scripts/client.js
+++ Scripts/client.js
@@ -2,4 +2,10 @@ $( document ).ready( onReady );
 
 function onReady (){
     console.log( `JQuery is ready!`)
-}
+    // click event for a button that exists on scren. 
+    $( '#newHotDogButton').on( 'click', newHotDog )
+} // end onReady
+
+function newHotDog(){
+    console.log( 'in newHotDog')
+};
